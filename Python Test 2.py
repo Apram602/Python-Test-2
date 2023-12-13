d80f3cadaebd5edfb1f7a523a3d41b23b1c11e37
Aprampal <aprampal2442@gmail.com>
2023-12-12 22:31:32 -0500

    version 3

--- Python Test 2.py
+++ Python Test 2.py
@@ -17,22 +17,26 @@ while x !=6:
             print("Name already exists in the directory.")
             user = input("Do you want to update the name? (yes/no): ").lower()
             if user == 'yes':
-               print("Your name is updated")
+                new_name = input("Enter the new name: ")
+                phoneDirectory[new_name] = phoneDirectory[name]
+                del phoneDirectory[name]
+                print("Your name is updated")
             elif user == 'no':
-               print("The name is not updated")
-        if name.isalpha():
-            number = input("Enter your 10-digit phone number: ")
-            if number.isnumeric and len(number) == 10:
-                phoneDirectory[name] = {"Number": number}
-                print("\nRecord Added\n")
-                print("Name:", name)
-                print("Number:", number)
-                print("\n")
-                print("Your Contacts",phoneDirectory)
-            else:
-                print("Please enter a 10-digit numeric phone number.")
+                print("The name is not updated")
         else:
-            print("Please enter a valid name with alphabet only.")
+            if name.isalpha():
+                number = input("Enter your 10-digit phone number: ")
+                if number.isnumeric and number >= 9999999999:
+                    phoneDirectory[name] = {"Number": number}
+                    print("\nRecord Added\n")
+                    print("Name:", name)
+                    print("Number:", number)
+                    print("\n")
+                    print("Your Contacts",phoneDirectory)
+                else:
+                    print("Please enter a 10-digit numeric phone number.")
+            else:
+                print("Please enter a valid name with alphabet only.")
  
     elif x==2:
         search=input("Enter name to search: ")
@@ -41,18 +45,26 @@ while x !=6:
         else:
             print("No contact exists with this name.")
 
-    elif x==3:
-        update_data1 =input("Enter Name to update: ")
-        if update_data1  in phoneDirectory:
-            name=input("Enter new Name: ")
-            number = int(input("Enter your New 10-digit phone number: "))
-            phoneDirectory.update({name:number})
-            print("Updated", update_data1, "to :", name)
-            print("Updated",  "Number to :", number)
-            if phoneDirectory =={}:
+    elif x == 3:
+        update_data1 = input("Enter Name to update: ")
+        if update_data1 in phoneDirectory:
+            new_name = input("Enter new Name: ")
+            if new_name.isalpha():
+                new_number = input("Enter your New 10-digit phone number: ")           
+                if new_number.isnumeric() and len(new_number) == 10:
+                   updated_contact = {new_name: {"Number": new_number}}
+                   phoneDirectory.update(updated_contact)
+                   del phoneDirectory[update_data1]
+                   print("Updated", update_data1, "to:", new_name)
+                   print("Updated Number to:", new_number)
+                else:
+                   print("Please enter a valid 10-digit numeric phone number.")
+            else:
+                print("Please enter a valid name with alphabetic characters only.")   
+        elif phoneDirectory =={}:
                 print("Your Phone Directory is Empty")
         else:
-            print("No data not found")
+            print("No data found for", update_data1)
 
     elif x==4:
         if phoneDirectory =={}:
@@ -67,16 +79,17 @@ while x !=6:
             print("Name:", name)
             print("Number:", number['Number'])
 
-    elif x ==5:
-        if phoneDirectory =={}:
-            print("Your Phone Directory is Empty")
+    elif x == 5:
+        if phoneDirectory == {}:
+           print("Your Phone Directory is Empty")
         else:
-            delete = input("Enter Contact Name to delete from records: ")
-            if delete.isalpha in phoneDirectory:
-                del phoneDirectory[delete]
-                print("Deleted", delete)
-            else:
-                print("Please Enter the in Alphabet")
+           delete = input("Enter Contact Name to delete from records: ")
+        if delete.isalpha() and delete in phoneDirectory:
+            del phoneDirectory[delete]
+            print("Deleted", delete)
+        else:
+            print("Please Enter a valid Contact Name in Alphabet or the name does not exist")
+
 
 
     print("")
